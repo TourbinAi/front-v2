e16c88e9014eabe39f30c7d2139540e53d134ce7
aliSohani <a.sohani.shahrood@gmail.com>
2024-12-04 14:05:11 +0330

    new marketing rar

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Attractions } from "./_components/Attractions";
+import { FeatureDetails } from "./_components/FeatureDetails";
+import { Header } from "./_components/Header";
+import { Hero } from "./_components/Hero";
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <Header />
+      <Hero />
+      <FeatureDetails />
+      <Attractions />
+    </main>
+  );
+}
