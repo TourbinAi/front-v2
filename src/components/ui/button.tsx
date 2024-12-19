9503f4e85d8159bfd4c452451b4a4fd6f7c3a82e
aliSohani <a.sohani.shahrood@gmail.com>
2024-12-19 16:00:10 +0330

    fixed the button type error

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -55,7 +55,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         onClick={() => href && router.push(href)}
         {...props}
       />
-    );
+    ) as any;
   }
 );
 Button.displayName = "Button";
